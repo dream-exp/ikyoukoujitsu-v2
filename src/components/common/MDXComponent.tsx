4a7e28797de571cfd314fbbe4fe592f3bffaaaaf
Shunnosuke Kataoka <shunnosuke834@gmail.com>
2023-07-16 16:44:42 +0900

    fix

--- src/components/common/MDXComponent.tsx
+++ src/components/common/MDXComponent.tsx
@@ -1,14 +1,31 @@
 import Image from "next/image";
+import { useState } from "react";
+
+type OnLoadingCompleteResult = { naturalHeight: number; naturalWidth: number };
 
 const img = (props: any) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [aspectRatio, setAspectRatio] = useState(0);
+  const onLoadingComplete = (e: OnLoadingCompleteResult) => {
+    setAspectRatio(e.naturalWidth / e.naturalHeight);
+  };
+
   return (
-    <Image
-      {...props}
-      fill
-      placeholder="blur"
-      className="object-contain !relative w-auto"
-      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkkAQAAB8AG7jymN8AAAAASUVORK5CYII="
-    />
+    <span
+      style={{
+        aspectRatio: `${aspectRatio || 1.5}`,
+        position: "relative",
+      }}
+    >
+      <Image
+        {...props}
+        fill
+        placeholder="blur"
+        className="object-cover !relative w-auto"
+        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkkAQAAB8AG7jymN8AAAAASUVORK5CYII="
+        onLoadingComplete={(e) => onLoadingComplete(e)}
+      />
+    </span>
   );
 };
 
