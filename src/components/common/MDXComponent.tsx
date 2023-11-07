82ec76e2946b3cf66a0930e03e010015ccfe2bc2
Shunnosuke Kataoka <shunnosuke834@gmail.com>
2023-11-08 02:19:36 +0900

    fix

--- src/components/common/MDXComponent.tsx
+++ src/components/common/MDXComponent.tsx
@@ -1,18 +1,20 @@
 import Image from "next/image";
 
 const img = (props: any) => {
-  console.log(props);
   return (
     // <img
     //   {...props}
     //   loading="lazy"
-    //   className="animate-fade-in aspect-auto h-auto"
+    //   className="animate-fade-in object-contain"
+    //   placeholder="empty"
     // />
     <Image
       {...props}
-      width={2000}
-      height={1333}
-      className="object-cover !relative w-auto animate-fade-in"
+      className="object-contain !relative w-auto h-auto animate-fade-in"
+      fill
+      placeholder="blur"
+      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjuHXr1n8ACK4DjtGRIJ0AAAAASUVORK5CYII="
+      unoptimized
     />
   );
 };
--- src/components/common/PostLayout.tsx
+++ src/components/common/PostLayout.tsx
@@ -1,11 +1,14 @@
 import { MDXProvider } from "@mdx-js/react";
 import Link from "next/link";
-import { Suspense, useEffect, useState } from "react";
 
 import getFormattedDate from "@/utils/getFormattedDate";
 
 import Footer from "./Footer";
 import components from "./MDXComponent";
+
+const customComponents = {
+  img: components.img,
+};
 
 type Props = {
   children: React.ReactNode;
@@ -14,22 +17,11 @@ type Props = {
 
 const PostLayout: React.FC<Props> = (props) => {
   const { children, meta } = props;
-  const [isVisible, setIsVisible] = useState(false);
-
-  const date = new Date(meta.date);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsVisible(true);
-    }, 200); // 表示までの遅延時間（ミリ秒）
-
-    return () => clearTimeout(timer); // コンポーネントがアンマウントされるときにタイマーをクリアする
-  }, []);
 
   return (
-    <MDXProvider components={components}>
+    <MDXProvider components={customComponents}>
       <div className="w-screen flex flex-col items-center">
-        <div className="w-[100%] max-w-[1200px] flex flex-col items-center gap-20 my-[100px]">
+        <div className="w-[100%] max-w-[1000px] flex flex-col items-center gap-20 my-[100px]">
           <Link href="/">
             <h1 className="text-3xl font-thin tracking-[0.7em] pl-6 transition-all hover:opacity-70">
               異郷好実
@@ -43,7 +35,7 @@ const PostLayout: React.FC<Props> = (props) => {
               <h2 className="w-full ext-xl font-bold">{meta.Title}</h2>
             </div>
             <div className="article-content flex flex-col items-center">
-              <Suspense fallback={<>読み込み中</>}>{children}</Suspense>
+              {children}
             </div>
           </div>
         </div>
